a157b820dce371dda99b5d67e06150ea311f1a7f
sanjin <2206585405@qq.com>
2022-04-14 17:07:12 +0800

    1111111111111

---
+++ src/akk.java
@@ -0,0 +1,8 @@
+import com.alibaba.druid.support.json.JSONUtils;
+
+public class akk {
+    public static void main(String[] args) {
+        String json = "{\"name\":\"zhangsan\",\"age\":20}";
+        System.out.println(JSONUtils.toJSONString(json));
+    }
+}
